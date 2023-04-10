855838aecd49b786a25765709c2349fcfe9acc50
trainerdev <fcadev@outlook.com>
2023-04-10 23:20:16 +0300

    - View binding, retrofit and moshi have been added to the project.

--- app/src/main/java/com/fcadev/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/fcadev/rickandmortyapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fcadev.rickandmortyapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
