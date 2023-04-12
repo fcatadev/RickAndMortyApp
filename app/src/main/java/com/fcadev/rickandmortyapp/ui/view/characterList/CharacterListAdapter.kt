f68fdb7e4210559284343c8a5982447bed5fc4fb
trainerdev <fatihcane0696@outlook.com>
2023-04-12 21:44:42 +0300

    - Added gender logo that changes by gender.

--- app/src/main/java/com/fcadev/rickandmortyapp/ui/view/characterList/CharacterListAdapter.kt
+++ app/src/main/java/com/fcadev/rickandmortyapp/ui/view/characterList/CharacterListAdapter.kt
@@ -5,19 +5,21 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.fcadev.rickandmortyapp.R
 import com.fcadev.rickandmortyapp.databinding.CharacterItemRowBinding
 import com.fcadev.rickandmortyapp.model.character.CharacterResult
 
-class CharacterListAdapter(private val characterList : ArrayList<CharacterResult>) :
-    RecyclerView.Adapter<CharacterListAdapter.CharacterListViewHolder>(){
+class CharacterListAdapter(private val characterList: ArrayList<CharacterResult>) :
+    RecyclerView.Adapter<CharacterListAdapter.CharacterListViewHolder>() {
 
-    class CharacterListViewHolder (var binding: CharacterItemRowBinding) :
-        RecyclerView.ViewHolder(binding.root){
+    class CharacterListViewHolder(var binding: CharacterItemRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterListViewHolder {
-        val binding = CharacterItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            CharacterItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CharacterListViewHolder(binding)
     }
 
@@ -30,7 +32,18 @@ class CharacterListAdapter(private val characterList : ArrayList<CharacterResult
             .into(holder.binding.ivCharacterPicture)
 
         holder.binding.tvNameText.text = characterItem.name
-        holder.binding.tvGenderText.text = characterItem.gender
+
+        when (characterItem.gender) {
+            "Male" -> {
+                holder.binding.ivGenderLogo.setImageResource(R.drawable.male)
+            }
+            "Female" -> {
+                holder.binding.ivGenderLogo.setImageResource(R.drawable.femenine)
+            }
+            else -> {
+                holder.binding.ivGenderLogo.setImageResource(R.drawable.planet)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -38,7 +51,7 @@ class CharacterListAdapter(private val characterList : ArrayList<CharacterResult
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    fun updateCharacterList(newCharacterList: MutableList<CharacterResult>){
+    fun updateCharacterList(newCharacterList: MutableList<CharacterResult>) {
         characterList.clear()
         characterList.addAll(newCharacterList)
         notifyDataSetChanged()
